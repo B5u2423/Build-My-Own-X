1b6536a41c87c6502eb8651927b52e558b07a75c
B5u2423 <masterofyordle@gmail.com>
2025-02-26 16:03:56 +0700

    c-database: commands

--- c-database/main.c
+++ c-database/main.c
@@ -5,17 +5,33 @@
 int main(int argc, char **argv) 
 {
     InputBuffer *input_buffer = new_input_buffer();
+    Statement statement;
     while (true) {
         print_prompt();
         read_input_buffer(input_buffer);
 
-        if (strcmp(input_buffer->buffer, ".exit") == 0) {
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
-        } else {
-            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
-            exit(EXIT_FAILURE);
+        if (input_buffer->buffer[0] == '.') {
+            switch (do_meta_command(input_buffer)) {
+            case META_COMMAND_SUCCESS:
+                exit(EXIT_SUCCESS);
+            case META_COMMAND_FAILURE:
+                printf("Error unrecognized command: '%s'\n", 
+                    input_buffer->buffer);
+                continue;
+            }
         }
+
+        switch (prepare_statement(input_buffer, &statement)) {
+        case PREPARE_SUCCESS:
+            break;
+        case PREPARE_FAILURE:
+            printf("Error unrecognized command: '%s'\n", 
+                input_buffer->buffer);
+            continue;;
+        }
+
+        execute_statement(&statement);
+        continue;
     }
 
 }
--- c-database/repl.c
+++ c-database/repl.c
@@ -2,12 +2,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <aio.h>
+#include <string.h>
 
 typedef struct {
     char *buffer;
     size_t buffer_length;
     ssize_t input_length;
 } InputBuffer;
+
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_FAILURE
+} MetaCommandResult;
+
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_FAILURE
+} PrepareResult;
+
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
+
+typedef struct {
+    StatementType type;
+} Statement;
+
+void insert_statement();
+void select_statement();
 
 InputBuffer *new_input_buffer()
 {
@@ -45,3 +68,45 @@ void read_input_buffer(InputBuffer *input_buffer)
 
 void print_prompt() { printf("mysqlite > "); }
 
+MetaCommandResult do_meta_command(InputBuffer *input_buffer)
+{
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        exit(EXIT_SUCCESS);
+    }
+    return META_COMMAND_FAILURE;
+}
+
+StatementType prepare_statement(InputBuffer *input_buffer, Statement *statement) {
+    if (strncmp(input_buffer->buffer, "select", 6) == 0) {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+    if (strcmp(input_buffer->buffer, "insert") == 0) {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    return PREPARE_FAILURE;
+}
+
+void execute_statement(Statement *statement)
+{
+    switch (statement->type)
+    {
+    case (STATEMENT_INSERT):
+        insert_statement();
+        break;
+    case (STATEMENT_SELECT):
+        select_statement(); 
+        break;
+    }
+}
+
+void insert_statement()
+{
+    puts("do INSERT something");
+}
+
+void select_statement()
+{
+    puts("do SELECT something");
+}
